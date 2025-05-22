d08b404e9c11560353520c95c8732fd0f1a99af5
koncptai-dev <dev.koncptai@gmail.com>
2025-05-22 19:55:08 +0530

    console env

--- src/server.js
+++ src/server.js
@@ -2,7 +2,14 @@ const app = require('./app');
 require('dotenv').config();
 
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
+console.log("PORT => ",process.env.PORT)
+console.log("DB_NAME => ",process.env.DB_NAME)
+console.log("DB_USER => ",process.env.DB_USER)
+console.log("DB_PASSWORD => ",process.env.DB_PASSWORD)
+console.log("DB_HOST => ",process.env.DB_HOST)
+console.log("PORT => ",process.env.PORT)
+console.log(process.env.PORT)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
