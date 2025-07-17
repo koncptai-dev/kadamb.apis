677859d931092e11dd29a4b8065a28960d5e372f
vrutti631 <vrutti.patel@koncpt.ai>
2025-07-17 19:04:16 +0530

    17th  july changes

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ app.use(cors());
 app.use(bodyParser.json());
 const path = require('path');
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.use('/api/agent', agent);
 app.use('/api/officeagent', officeAgent); // Use Office Agent Routes
